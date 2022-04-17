42c8f9c766749eb0870d8beb2d1438c8a5148d62
mo7amedehab97 <gazacityface@gmail.com>
2022-04-17 16:48:57 +0300

    update the change color

--- src/components/ChangeColor.jsx
+++ src/components/ChangeColor.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react';
 
 const ChangeColor = () => {
-  const [coords, setCoords] = useState({ x: 0, y: 0 });
+  const [coordinates, setCoordinates] = useState({ x: 0, y: 0 });
   const [backgroundColor, setBackgroundColor] = useState('#fff');
 
   useEffect(() => {
@@ -15,20 +15,20 @@ const ChangeColor = () => {
   const handleChangeColor= (e) => {
     const screenWidth = window.innerWidth / 2;
 
-    if (coords.x > screenWidth) {
+    if (coordinates.x > screenWidth) {
       setBackgroundColor('blue')
     } else {
       setBackgroundColor('red');
 
     }
 
-    setCoords({ x: e.clientX, y: e.clientY });
+    setCoordinates({ x: e.clientX, y: e.clientY });
   };
 
   return (
     <>
       <h1 style={{ backgroundColor: backgroundColor  }}>
-        I'm now on X: {coords.x}, and Y: {coords.y}
+        I'm now on X: {coordinates.x}, and Y: {coordinates.y}
       </h1>
     </>
   );
